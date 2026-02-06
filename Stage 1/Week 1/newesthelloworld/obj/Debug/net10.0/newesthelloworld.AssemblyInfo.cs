d46210ba56bf0d11aa5e725de830a2e717d05600
Cooper Brown <cooperbrown.dev@gmail.com>
2026-02-06 10:23:48 -0600

    Test commit message

--- Stage 1/Week 1/newesthelloworld/obj/Debug/net10.0/newesthelloworld.AssemblyInfo.cs
+++ Stage 1/Week 1/newesthelloworld/obj/Debug/net10.0/newesthelloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("newesthelloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0ad8967b8d4decf56e3d8fc5c3b7ea82d220148")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+741261ed42b4b48e64f99c5765f4c58168dd7354")]
 [assembly: System.Reflection.AssemblyProductAttribute("newesthelloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("newesthelloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
