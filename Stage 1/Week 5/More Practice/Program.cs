e8e5fe0939c11276f94b2e66b5b200c914d34f97
Cooper Brown <cooperbrown.dev@gmail.com>
2026-02-06 10:10:18 -0600

    Practice programs

---
+++ Stage 1/Week 5/More Practice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Week5MorePractice;
+class Orc
+{
+    // STATIC: This belongs to the "Blueprint." 
+    // There is only ONE of these for the whole game.
+    public int TotalOrcCount = 0;
+    // NON-STATIC: This belongs to the "House." 
+    // Every Orc has its own unique health.
+    public int health = 100;
+    public Orc()
+    {
+        // Every time 'new Orc()' is called, we increment the shared counter
+        TotalOrcCount++;
+    }
+    public void TakeDamage(int amount)
+    {
+        // This only affects the health of the SPECIFIC orc that was hit
+        health -= amount;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = -5;
+        Console.WriteLine(Math.Abs(num));
+    } //end of main
+    
+} // end of program
+
---
+++ Stage 1/Week 5/Practice/MorePractice.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Week5Practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    
+        string textMoney = "14.99";
+        decimal dMoney = decimal.Parse(textMoney);
+        Console.WriteLine(dMoney);
+
+
+    }
+
+    
+        
+        
+        
+    } //end of main
+}
