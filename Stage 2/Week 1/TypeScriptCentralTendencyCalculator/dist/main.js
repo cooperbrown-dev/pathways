74472282fa54ec28fd0e55b6ccf8fb750e8686e9
Cooper Brown <cooperbrown.dev@gmail.com>
2026-02-12 13:29:23 -0600

    first commit

--- Stage 2/Week 1/TypeScriptCentralTendencyCalculator/dist/main.js
+++ Stage 2/Week 1/TypeScriptCentralTendencyCalculator/dist/main.js
@@ -1,70 +1,63 @@
+"use strict";
 let numArray = [];
 let modes = [];
-
-function validateDisplayAndAddNumsToArray(){
-    let minValue = document.getElementById("minValue").value;
-    let maxValue = document.getElementById("maxValue").value;
+function validateDisplayAndAddNumsToArray() {
+    let minValue = Number(document.getElementById("minValue").value);
+    let maxValue = Number(document.getElementById("maxValue").value);
     let numInput = Number(document.getElementById("numInput").value);
-
-    if(numInput < minValue){
-        alert("You must enter a number greater than the minimum value provided.")
+    if (numInput < minValue) {
+        alert("You must enter a number greater than the minimum value provided.");
     }
-    else if(numInput > maxValue){
-        alert("You must enter a number less than the maximum value provided.")
+    else if (numInput > maxValue) {
+        alert("You must enter a number less than the maximum value provided.");
     }
-    else if(numInput == ""){
-        alert("Please enter a number between the minimum and maximum value.")
+    else if (numInput == null) {
+        alert("Please enter a number between the minimum and maximum value.");
     }
-    else{
+    else {
         //Add number to array and display the numbers entered to the user.
         numArray.push(numInput);
         document.getElementById("enteredNums").innerHTML += numInput + " ";
     }
 }
-
-function calculateMean(){
+function calculateMean() {
     let sum = numArray.reduce((acc, num) => acc + num, 0);
     let mean = sum / numArray.length;
-    document.getElementById("displayMean").innerHTML = mean;
+    document.getElementById("displayMean").innerHTML = mean.toString();
 }
-
-function calculateMedian(){
+function calculateMedian() {
     let median;
     let orderedArray = numArray.sort((a, b) => a - b);
     let middleOfArray = Math.floor(orderedArray.length / 2);
-    
-    if(orderedArray.length % 2 == 0){
+    if (orderedArray.length % 2 == 0) {
         median = (orderedArray[middleOfArray - 1] + orderedArray[middleOfArray]) / 2;
     }
-    else{
+    else {
         median = orderedArray[middleOfArray];
     }
-    document.getElementById("displayMedian").innerHTML = median;
+    document.getElementById("displayMedian").innerHTML = median.toString();
 }
-
-function calculateMode(){
+function calculateMode() {
     let maxCount = 0;
-    
-    for (let i = 0; i < numArray.length; i++){
+    for (let i = 0; i < numArray.length; i++) {
         let count = 0;
-        for (let j = 0; j < numArray.length; j++){
-            if (numArray[j] === numArray[i]){
+        for (let j = 0; j < numArray.length; j++) {
+            if (numArray[j] === numArray[i]) {
                 count++;
             }
         }
-        
-        if (count > maxCount){
+        if (count > maxCount) {
             maxCount = count;
             modes.length = 0;
             modes.push(numArray[i]);
-        } else if(count === maxCount && !modes.includes(numArray[i])){
-            modes.push(numArray[i])
+        }
+        else if (count === maxCount && !modes.includes(numArray[i])) {
+            modes.push(numArray[i]);
         }
     }
-    document.getElementById("displayMode").innerHTML = modes.join(" ")
+    document.getElementById("displayMode").innerHTML = modes.join(" ");
 }
-
-function clearNums(){
+function clearNums() {
     numArray = [];
     modes = [];
     document.getElementById("minValue").value = "";
---
+++ Stage 2/Week 1/TypeScriptCentralTendencyCalculator/main.ts
@@ -0,0 +1,77 @@
+let numArray: number[] = [];
+let modes: number[] = [];
+
+function validateDisplayAndAddNumsToArray(){
+    let minValue: number = Number((document.getElementById("minValue") as HTMLInputElement).value);
+    let maxValue: number = Number((document.getElementById("maxValue") as HTMLInputElement).value);
+    let numInput: number = Number((document.getElementById("numInput") as HTMLInputElement).value);
+
+    if(numInput < minValue){
+        alert("You must enter a number greater than the minimum value provided.")
+    }
+    else if(numInput > maxValue){
+        alert("You must enter a number less than the maximum value provided.")
+    }
+    else if(numInput == null){
+        alert("Please enter a number between the minimum and maximum value.")
+    }
+    else{
+        //Add number to array and display the numbers entered to the user.
+        numArray.push(numInput);
+        (document.getElementById("enteredNums") as HTMLSpanElement).innerHTML += numInput + " ";
+    }
+}
+
+function calculateMean(){
+    let sum: number = numArray.reduce((acc, num) => acc + num, 0);
+    let mean: number = sum / numArray.length;
+    (document.getElementById("displayMean") as HTMLSpanElement).innerHTML = mean.toString();
+}
+
+function calculateMedian(){
+    let median: number;
+    let orderedArray: number[] = numArray.sort((a, b) => a - b);
+    let middleOfArray: number = Math.floor(orderedArray.length / 2);
+    
+    if(orderedArray.length % 2 == 0){
+        median = (orderedArray[middleOfArray - 1] + orderedArray[middleOfArray]) / 2;
+    }
+    else{
+        median = orderedArray[middleOfArray];
+    }
+    (document.getElementById("displayMedian") as HTMLSpanElement).innerHTML = median.toString();
+}
+
+function calculateMode(){
+    let maxCount: number = 0;
+    
+    for (let i: number = 0; i < numArray.length; i++){
+        let count: number = 0;
+        for (let j: number = 0; j < numArray.length; j++){
+            if (numArray[j] === numArray[i]){
+                count++;
+            }
+        }
+        
+        if (count > maxCount){
+            maxCount = count;
+            modes.length = 0;
+            modes.push(numArray[i]);
+        } else if(count === maxCount && !modes.includes(numArray[i])){
+            modes.push(numArray[i])
+        }
+    }
+    (document.getElementById("displayMode") as HTMLSpanElement).innerHTML = modes.join(" ")
+}
+
+function clearNums(){
+    numArray = [];
+    modes = [];
+    (document.getElementById("minValue") as HTMLInputElement).value = "";
+    (document.getElementById("maxValue") as HTMLInputElement).value = "";
+    (document.getElementById("numInput") as HTMLInputElement).value = "";
+    (document.getElementById("enteredNums") as HTMLSpanElement).innerHTML = "";
+    (document.getElementById("displayMean") as HTMLSpanElement).innerHTML = "";
+    (document.getElementById("displayMedian") as HTMLSpanElement).innerHTML = "";
+    (document.getElementById("displayMode") as HTMLSpanElement).innerHTML = "";
+}
