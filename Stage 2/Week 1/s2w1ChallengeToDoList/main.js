1e17c27d14802d091053ab5ade67e8d875073a77
Cooper Brown <cooperbrown.dev@gmail.com>
2026-02-12 16:56:55 -0600

    Added due date / time

--- Stage 2/Week 1/s2w1ChallengeToDoList/main.js
+++ Stage 2/Week 1/s2w1ChallengeToDoList/main.js
@@ -11,7 +11,9 @@ function addToToDoList(){
         let cell1 = row.insertCell();
         let cell2 = row.insertCell();
         let cell3 = row.insertCell();
+        let cell4 = row.insertCell();
 
+        // add a checkbox and strikethrough when checkbox is checked
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.name = "completed";
@@ -20,19 +22,24 @@ function addToToDoList(){
         });
         cell1.appendChild(checkbox);
 
+        // add the task to do
         cell2.innerHTML = newListItem;
 
+        // add a due date/time for when the task is to be completed
+        let dateAndTimeDue = document.createElement("input");
+        dateAndTimeDue.type = "datetime-local";
+        cell3.appendChild(dateAndTimeDue);
+        
+        // add a delete button to delete that row
         let deleteButton = document.createElement("button");
         deleteButton.textContent = "Delete";
         deleteButton.value = "Delete";
         deleteButton.addEventListener("click", function(){
             row.remove();
         });
-        cell3.appendChild(deleteButton);
+        cell4.appendChild(deleteButton);
 
-        //add due date?
-        //cell4
-
+        // reset the input box
         document.getElementById("newListItem").value = "";
     }
 }
